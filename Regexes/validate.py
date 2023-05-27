7636c16ac534eb6e5adcb0f6932f9078c02be230
Benjamin <benjamin.jugendfeuerwehr@gmail.com>
2023-05-27 02:37:13 +0200

    1 hour more

--- Regexes/validate.py
+++ Regexes/validate.py
@@ -1,10 +1,32 @@
 """docs.python.org/3/library/re.html
    re.search(pattern, string, flags=0)
+   "." any character except a newline
+   "*" 0 or more repetitions
+   "+" 1 more repetition
+   "?" 0 or 1 repetition
+   "{m}" m repetitions
+   "{m, n}" m to n repetitions
+   
+   ^ matches start of string
+   $ matches end of string before newline
+   [] set of characters
+   [^] complementing the set
+   
+   \d decimal digit
+   \s whitespace characters
+   \w word character ... as well as numbers and the underscore
+   uppercase = opposite
+   
+   A|B either A or B
+   (...) a group
+   (?:...) non-capturing version
+   
+   r regular expresion
 """
 import re
 email = input("What's your email? ").strip()
 
-if re.search("@", email):
+if re.search(r"^\w+@\w+\.(com|edu)$", email, re.IGNORECASE):
     print("Valid")
 else:
     print("Invalid")
