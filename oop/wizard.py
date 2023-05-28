272ee0403d9675972eff68bdeb1880d7965243f0
Benjamin <benjamin.jugendfeuerwehr@gmail.com>
2023-05-29 01:10:47 +0200

    2last

--- oop/wizard.py
+++ oop/wizard.py
@@ -1,14 +1,27 @@
-class Student:
-    def __init__(self, name, house):
+class Wizard:
+    def __init__(self, name):
+        if not name:
+            raise ValueError("Missing name")
         self.name = name
+    
+    ...
+
+class Student(Wizard):
+    def __init__(self, name, house):
+        super().__init__(name)
         self.house = house
     
     ...
 
 
-class Professor:
+class Professor (Wizard):
     def __init__(self, name, subject):
-        self.name = name
-        self.subject =subject
+        super().__init__(name)
+        self.subject = subject
         
     ...
+    
+
+wizard = Wizard("Albus")
+student = Student("Harry", "Gryffindor")
+professor = Professor("Serverus", "Defense Against the Dark Arts")
