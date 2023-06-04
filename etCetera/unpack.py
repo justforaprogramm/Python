e2b6e197043925abbee8eb9303a2daa7f21231a4
Benjamin <benjamin.jugendfeuerwehr@gmail.com>
2023-06-04 12:23:39 +0200

    end

--- etCetera/unpack.py
+++ etCetera/unpack.py
@@ -1,5 +1,13 @@
-def total(galleons, sickless, knuts):
+#            default 0, kann verändert werden       
+#                  v           v        v
+def total(galleons=0, sickless=0, knuts=0):
     return (galleons * 17 + sickless) * 29 + knuts
 
+#liste
 coins = [100, 50, 25]
 print(total(*coins), "Knuts")
+
+#wörterbuch
+coins = {"galleons": 100, "sickless": 50, "knuts": 25}
+
+print(total(**coins), "knuts")
