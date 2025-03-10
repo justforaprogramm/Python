1b797d40942c23501d67d86fea30abd18c3008d7
Benjamin <benjamin.jugendfeuerwehr@gmail.com>
2025-03-10 19:17:24 +0100

    pygame continue

--- pygames/pong_start.py
+++ pygames/pong_start.py
@@ -32,31 +32,46 @@ def Player2(player:int, keys) -> int:
     return player 
 
 def Ball(xpos, ypos):
-    if xpos < 0:
-        print('Player1 Won!')
-    if xpos > 1920:
-        print('player2 Won!')
-    return xpos, ypos
 
+    y = ''
+    if xpos < 0:
+        y = 'Player1 Won!'
+    if xpos > 1920:
+        y = 'player2 Won!'
+    return xpos, ypos, y
+
+def end(p):
+    l:bool = True
+    while l:
+        screen.fill((0, 0, 0))
+        print(p)
+        if keys[pygame.K_w] and keys[pygame.K_UP]:
+            l = False
+        pygame.display.flip()
+
+winner:str = 'W and Up to Start'
 run:bool = True
 while run:
-    
+    keys = pygame.key.get_pressed()
     screen.fill((0, 0, 0))
     
+
     # makes the square
     pygame.draw.line(screen, (255, 0, 0), [0, y1], [0, (y1 + 150)], 5)
     pygame.draw.line(screen, (255, 0, 0), [1920, y2], [1920, (y2 + 150)], 5)
     pygame.draw.circle(screen, (255, 255, 255), [x3,y3], radius=25, width=0)
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False
-
-    keys = pygame.key.get_pressed()
+    if winner:
+        end(winner)
+        winner = ''
 
     y1 = Player1(y1, keys)
     y2 = Player2(y2, keys)
-    x3, y3 = Ball(x3, y3)
+    x3, y3, winner = Ball(x3, y3)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
 
     pygame.display.flip()
 
