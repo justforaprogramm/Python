e6e03f301d7e8b4ade83f9854384fc74debe8675
Benjamin <benjamin.jugendfeuerwehr@gmail.com>
2025-04-01 21:57:10 +0200

    formal korektness

--- WledRequest/toggle.py
+++ WledRequest/toggle.py
@@ -3,18 +3,15 @@ import json
 import argparse
 
 class WLEDController:
-    def __init__(self, ip="192.168.2.115"):
-        """Initialisiert die Klasse mit der festen WLED-IP-Adresse."""
-        self.url = f"http://{ip}/json/state"
+    def __init__(self):
+        self.url = f"http://192.168.2.115/json/state"
         self.http = urllib3.PoolManager()
 
     def get_state(self):
-        """Holt den aktuellen Zustand von WLED."""
         response = self.http.request('GET', self.url)
         return json.loads(response.data.decode('utf-8'))
 
     def toggle(self, debug=False):
-        """Schaltet WLED um (Ein/Aus)."""
         state = self.get_state()
         new_state = not state.get("on", False)
 
@@ -22,7 +19,7 @@ class WLEDController:
         response = self.http.request(
             'POST',
             self.url,
-            body=json.dumps(payload),
+            body = json.dumps(payload),
             headers={'Content-Type': 'application/json'}
         )
 
