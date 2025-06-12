275a9a82565d3e0dc097140926603e567c14e553
Benjamin <benjamin.jugendfeuerwehr@gmail.com>
2025-06-12 03:42:19 +0200

    smaller version of wled toggle

--- WledRequest/toggle_small.py
+++ WledRequest/toggle_small.py
@@ -4,5 +4,5 @@ from json import dumps, loads
 PoolManager().request(
     'POST',
     "http://192.168.2.100/json/state",
-    dumps({"on": not loads(PoolManager().request('GET', "http://192.168.2.100/json/state").get("on", False)})
+    dumps({"on": not loads(PoolManager().request('GET', "http://192.168.2.100/json/state").data.decode('utf-8')).get("on", False)})
 )
