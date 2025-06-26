2f3255110cbe09d0aae8bb48ac655cf49307cd20
Benjamin <benjamin.jugendfeuerwehr@gmail.com>
2025-06-26 17:03:21 +0200

    better

--- little_things/pyramide.py
+++ little_things/pyramide.py
@@ -1,6 +1,7 @@
-j = 7
-for i in range(1, j+1, 2):
-    print(' ' * (j-(i//2)) ,'*' * i, sep='')
+line_between = 1
+max_witdh = 10
+for i in range(1, max_witdh+1, 2):
+    print(' ' * (max_witdh-(i//2)) ,'*' * i, sep='', end=('\n'*line_between if line_between > 0 else '\n'))
 
-for i in reversed(range(1, j+1, 2)):
-    print(' ' * (j-(i//2)) ,'*' * i, sep='')
+for i in reversed(range(1, max_witdh+1, 2)):
+    print(' ' * (max_witdh-(i//2)) ,'*' * i, sep='', end=('\n'*line_between if line_between > 0 else '\n'))
