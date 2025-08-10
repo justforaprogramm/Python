44e301cfb74ed7d7e5399d9e927e1da57ea1ae8d
Benjamin <benjamin.jugendfeuerwehr@gmail.com>
2025-08-10 18:25:11 +0200

    ll

--- versuch.py
+++ versuch.py
@@ -7,13 +7,15 @@
 #if __name__ == "__main__":    
 #    main()
 
-class main():
-    def __init__(self):
-        min = sec / 60
-        sec = sec % 60
-        print(f"minuten: {int(min)} \n secunden: {sec}")
-    def input(self):
-        self.sec = int(input("Sekunden: "))
+# class main():
+#     def __init__(self):
+#         min = sec / 60
+#         sec = sec % 60
+#         print(f"minuten: {int(min)} \n secunden: {sec}")
+#     def input(self):
+#         self.sec = int(input("Sekunden: "))
         
-if __name__ == "__main__":    
-    main()
+# if __name__ == "__main__":    
+#     main()
+
+print('yes') if '' == "" else print('no')
