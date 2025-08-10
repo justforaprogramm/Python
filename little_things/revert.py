531b867718cb3e1f59ec3a74aac6aacd8e9acb85
Benjamin <benjamin.jugendfeuerwehr@gmail.com>
2025-08-10 18:55:19 +0200

    nice

--- little_things/revert.py
+++ little_things/revert.py
@@ -1,6 +1,19 @@
+'''https://www.mit.edu/~ecprice/wordlist.10000, https://lingojam.com/TexttoOneLine
+'''
+from pathlib import Path
+
 def main(sequence) -> str:
     return '\x20'.join(reversed([i for i in sequence.split('\x20') if i]))
 
-if '__main__' == __name__:
-    sequence:str = input('enter a sentence: ')
-    print(main(sequence))
+if __name__ == '__main__':
+    script_dir = Path(__file__).parent
+    file_path = script_dir / "rev.txt"
+    out_path = script_dir / "out.txt"
+
+    with open(file_path, "r", encoding="utf-8") as f:
+        sequence = f.readline().strip()
+
+    result = main(sequence)
+
+    with open(out_path, "w", encoding="utf-8") as f:
+        f.write(result)
