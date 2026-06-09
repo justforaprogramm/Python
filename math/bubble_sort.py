e1ca0e2c0e442abe3288ddefcc5998c665a96307
Justforaprogramm <benjamin.jugendfeuerwehr@gmail.com>
2026-06-09 15:34:51 +0200

    yay

--- math/bubble_sort.py
+++ math/bubble_sort.py
@@ -9,11 +9,18 @@ def bubble_sort(arr:list(int)) -> list(int):
     return:
         _type_ : sorted list
     """
+    no_swap:int = 0
     for _ in range(len(arr)-1):
+        if no_swap == len(arr):
+            return arr
+        no_swap = 0
         for i in range(len(arr)-1):
             if arr[i] > arr[i+1]:
                 arr[i], arr[i+1] = arr[i+1], arr[i]
+                no_swap = no_swap + 1
     return arr
 
 print(bubble_sort([3,5,2,4,1]))
+# print(bubble_sort(list(range(0, 10000))))
+# print(bubble_sort(list(range(10000, 0, -1))))
 # hat n² weil die innere ist n und außen auch n
